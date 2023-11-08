246fd051963526d998352482edefde508d472b04
Mahammad-Rehan <149800722+Mahammad-Rehan@users.noreply.github.com>
2023-11-08 09:40:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import './App.css'
 
-function App() {
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().required().email(),
+  password: yup.string().required().min(8),
+  country: yup.string().required(),
+  gender: yup.string().required(),
+});
+
+function MyForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const onSubmit = data => alert('Form was submitted: ' + JSON.stringify(data));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("name")} placeholder="Username" type='text' />
+      {errors.name && <p>This field is required</p>}
+      
+      <input {...register("email")} placeholder="Email" type='text'/>
+      {errors.email && <p>This field is required and should be a valid email</p>}
+      
+      <input {...register("password")} placeholder="Password" type="password" />
+      {errors.password && <p>This field is required and should be at least 8 characters long</p>}
+      
+
+      <select {...register("country")}>
+        <option value="" disabled>Select Country</option>
+        <option value="india">India</option>
+        <option value="usa">USA</option>
+      </select>
+      {errors.country && <p>This field is required</p>}
+      <br></br>
+      <br></br>
+      <input type="radio" value="Male" {...register("gender")} /> Male
+      <input type="radio" value="Female" {...register("gender")} /> Female
+      {errors.gender && <p>This field is required</p>}
+      
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
-export default App;
+export default MyForm;
